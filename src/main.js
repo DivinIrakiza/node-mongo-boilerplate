b55444dae2c3afb6a8eda932746e41df19055508
Divin Irakiza <irakizadivin@gmail.com>
2021-07-06 16:10:20 +0200

    feat: Added cors and body parset

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+
+
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cors());
+
+
+
 app.listen(PORT, () =>
-  console.log(```Running on port ${PORT} ```),
+  console.log(` Running on port ${PORT} `),
 );
