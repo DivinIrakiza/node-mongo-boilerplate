587a44b133953636012b6365c129d5ae9b210431
Divin Irakiza <irakizadivin@gmail.com>
2021-07-06 16:22:36 +0200

    setup: express adding loggers

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
+
+import path from 'path';
 import 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
- 
-
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-
-
-
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(bodyparser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('combined'))
 
+
+app.get('/', function (req, res) {
+    // return res.send('Jeesh! You reached the server 😊😊')
+    return res.sendFile(path.join(__dirname + '/src/templates/index.html'));
+});
 
 
 app.listen(PORT, () =>
